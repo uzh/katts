f6827345605d21d8837e213e73e24cdae52d8314
Lorenz Fischer <lorenz.fischer@gmail.com>
2013-05-01 21:24:55 +0200

    eval script and a some more evaluations

---
+++ src/dev/eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script does it all:
+#  download the json sendgraph from kraken
+#  convert json to metis
+#  partition metis file into 12 partitions
+#  simulate and compare performance
+#
+set -o verbose # print all commands
+scp lfischer@kraken.ifi.uzh.ch:~/katts_submission_environment/katts_jobs/$1/evaluation/$1.task.json $1.json
+./do_all.sh $1
